b98a6612ab10ee4ba0f4c899bcfeec45370fb88a
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-03-11 20:26:30 +0100

    added simple bar chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SimpleBarChart from "./components/SimpleBarChart";
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <SimpleBarChart/>
       </div>
     );
   }
