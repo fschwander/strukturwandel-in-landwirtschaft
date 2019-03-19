4740a0c9d049d2aeb9fc68d38352bd2c647cdb68
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-03-19 12:41:54 +0100

    added component with map of switzerland

--- src/components/MapOfSwitzerland.js
+++ src/components/MapOfSwitzerland.js
@@ -2,9 +2,56 @@ import * as React from 'react';
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
 import ch from '../res/ch'
+import swiss from '../res/ch-cantons'
 
 
 export default class MapOfSwitzerland extends React.Component {
+
+  drawMapWithCantonsAndLabels() {
+    let width = 960,
+      height = 600;
+
+    const projection = d3.geoAlbers()
+      .rotate([0, 0])
+      .center([8.3, 46.8])
+      .scale(16000)
+      .translate([width / 2, height / 2])
+      .precision(.1);
+
+    const path = d3.geoPath()
+      .projection(projection);
+
+    const svg = d3.select('.MapOfSwitzerland')
+      .append('g').attr('class', 'cantons-and-labels')
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    const cantons = topojson.feature(swiss, swiss.objects.cantons);
+
+    svg.append("path")
+      .datum(cantons)
+      .attr("class", "canton")
+      .attr("d", path);
+
+    svg.append("path")
+      .datum(topojson.mesh(swiss, swiss.objects.cantons, function (a, b) {
+        return a !== b;
+      }))
+      .attr("class", "canton-boundary")
+      .attr("d", path);
+
+    svg.selectAll("text")
+      .data(cantons.features)
+      .enter().append("text")
+      .attr("transform", function (d) {
+        return "translate(" + path.centroid(d) + ")";
+      })
+      .attr("dy", ".35em")
+      .text(function (d) {
+        return d.properties.name;
+      });
+  }
 
   /**
    * code from http://bl.ocks.org/herrstucki/4327678
@@ -28,17 +75,12 @@ export default class MapOfSwitzerland extends React.Component {
       .attr('d', path);
 
     svg.append('path')
-      .datum(topojson.mesh(ch, ch.objects.municipalities, function (a, b) {
-        return a !== b;
-      }))
+      .datum(topojson.mesh(ch, ch.objects.municipalities, (a, b) => a !== b))
       .attr('class', 'municipality-boundaries')
       .attr('d', path);
 
     svg.append('path')
-      .datum(topojson.mesh(ch, ch.objects.cantons, function (a, b) {
-        console.log(a, b);
-        return a !== b;
-      }))
+      .datum(topojson.mesh(ch, ch.objects.cantons, (a, b) => a !== b))
       .attr('class', 'canton-boundaries')
       .attr('d', path);
   }
@@ -48,6 +90,7 @@ export default class MapOfSwitzerland extends React.Component {
   }
 
   componentDidMount() {
+    // this.drawMapWithCantonsAndLabels();
     this.drawMapWithCantonsAndMunicipalities();
   }
 
