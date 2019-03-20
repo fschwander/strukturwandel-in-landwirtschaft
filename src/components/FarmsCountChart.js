b684cf5dd8c216dd74d57af8fc48aba3afbb8de8
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-03-20 10:20:36 +0100

    added own data

--- src/components/FarmsCountChart.js
+++ src/components/FarmsCountChart.js
@@ -1,52 +1,52 @@
 import * as React from "react";
 import * as d3 from "d3";
+import data from '../res/data/farm-sizes.csv'
 
 export default class FarmsCountChart extends React.Component {
 
   /**
    * Based on https://observablehq.com/@tmcw/stacked-area-chart
    */
-  drawChart() {
-    d3.csv("https://gist.githubusercontent.com/clhenrick/60b8de7fe07c0c6122cff4979feef1fe/raw/5f30c13b504b443bd4c027add21e79fbf1d8fa8d/pivot.csv",
+  init() {
+    d3.csv(data,
       d => ({
         year: +d.year,
-        'african american': +d['African American'],
-        asian: +d.Asian,
-        latino: +d['Latino'],
-        white: +d['White']
+        total_farms_count: +d.total_farms_count,
+        area_valley_in_percent: +d.area_valley_in_percent,
+        area_mountain_in_percent: +d.area_mountain_in_percent,
+        area_size_0_1: +d.area_size_0_1,
+        area_size_1_3: +d.area_size_1_3,
+        area_size_3_5: +d.area_size_3_5,
+        area_size_5_10: +d.area_size_5_10,
+        area_size_10_20: +d.area_size_10_20,
+        area_size_20_30: +d.area_size_20_30,
+        area_size_30_50: +d.area_size_30_50,
+        area_size_50_n: +d.area_size_50_n
       })).then(data => {
 
-      const width = 975;
-      const height = 400;
+      const width = 900;
+      const height = 500;
       const margin = {
-        top: 10,
+        top: 20,
         bottom: 60,
-        left: 65,
+        left: 80,
         right: 10
       };
       const innerHeight = height - margin.top - margin.bottom;
       const innerWidth = width - margin.left - margin.right;
 
+      const keys = Object.keys(data[0]).slice(1);
+      const stack = d3.stack().keys(keys);
+      const series = stack(data);
+      const colorScale = d3.scaleOrdinal(d3.schemePastel2);
+
       const xScale = d3.scaleLinear()
         .domain(d3.extent(data, d => d.year))
         .range([0, innerWidth]);
 
-      const keys = Object.keys(data[0]).slice(1);
-
-      const stack = d3.stack()
-        .keys(keys)
-        .order(d3.stackOrderDescending); // so that the largest grouping is stacked below the others
-
-      const series = stack(data);
-
       const yScale = d3.scaleLinear()
-        .domain([
-          d3.min(series, series => d3.min(series, d => d[0])),
-          d3.max(series, series => d3.max(series, d => d[1]))
-        ])
+        .domain([0, d3.max(series, series => d3.max(series, d => d[1]))])
         .range([innerHeight, 0]);
-
-      const colorScale = d3.scaleOrdinal(d3.schemePastel2);
 
       const xAxis = d3.axisBottom()
         .scale(xScale)
@@ -54,10 +54,13 @@ export default class FarmsCountChart extends React.Component {
         .tickFormat(d3.format(''));
 
       const yAxis = d3.axisLeft()
-        .scale(yScale)
-        .tickValues([0, 0.25, 0.5, 0.75, 1])
-        .tickFormat(d3.format(',.0%'));
+        .scale(yScale);
 
+      // define an area path generator
+      const area = d3.area()
+        .x(d => xScale(d.data.year))
+        .y0(d => yScale(d[0]))
+        .y1(d => yScale(d[1]));
 
       const svg = d3.select('.FarmsCountChart')
         .append('svg')
@@ -79,8 +82,8 @@ export default class FarmsCountChart extends React.Component {
         .append('text')
         .attr('class', 'label')
         .attr('x', innerWidth / 2)
-        .attr('y', 35)
-        .text('Year')
+        .attr('y', 40)
+        .text('Jahr')
         .style('text-anchor', 'middle');
 
       // generate y axis
@@ -89,19 +92,14 @@ export default class FarmsCountChart extends React.Component {
         .call(yAxis);
 
       // y axis label
-      d3.select('.y.axis').append('text')
+      d3.select('.y.axis')
+        .append('text')
         .attr('class', 'label')
         .attr('x', -innerHeight / 2)
-        .attr('y', -50)
-        .attr('transform', `rotate(-90 0 0)`)
-        .text('Ethnicity')
+        .attr('y', -60)
+        .attr('transform', 'rotate(-90)')
+        .text('Bauernhöfe')
         .style('text-anchor', 'middle');
-
-      // define an area path generator
-      const area = d3.area()
-        .x(d => xScale(d.data.year))
-        .y0(d => yScale(d[0]))
-        .y1(d => yScale(d[1]));
 
       // create the stacked area paths
       g.selectAll('.area')
@@ -114,7 +112,7 @@ export default class FarmsCountChart extends React.Component {
   }
 
   componentDidMount() {
-    this.drawChart()
+    this.init()
   }
 
   render() {
