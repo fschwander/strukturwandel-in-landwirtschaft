a6a99e07e4c83960190db54df5f54a8374949957
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-04-24 16:45:41 +0200

    refactoring of quick implemented code

--- src/components/FarmSizeRelationsChart.js
+++ src/components/FarmSizeRelationsChart.js
@@ -3,102 +3,38 @@ import * as d3 from "d3";
 
 export default class FarmSizeRelationsChart extends React.Component {
 
-  constructor(params) {
-    super(params);
-
-    this.state = {
-      data: this.props.data
-    }
-  }
-
+  /**
+   * Based on https://bl.ocks.org/mbostock/3887051
+   */
   drawChart() {
-    const {data} = this.state;
-    //
-    // const canvHeight = 600, canvWidth = 800;
-    // const margin = {top: 50, right: 20, bottom: 40, left: 80};
-    //
-    // const width = canvWidth - margin.left - margin.right;
-    // const height = canvHeight - margin.top - margin.bottom;
-    //
-    // const y = d3.scaleLinear()
-    //   .domain([0, d3.max(data, d => d.minYearData)])
-    //   .rangeRound([height, 0]);
-    //
-    // const x = d3.scaleBand()
-    //   .domain(data.map(d => d.label))
-    //   .rangeRound([0, width])
-    //   .padding(0.1);
-    //
-    // const svg = d3.select(".FarmSizeRelationsChart").append("svg")
-    //   .attr("width", canvWidth)
-    //   .attr("height", canvHeight);
-    //
-    // const g = svg.append('g')
-    //   .attr('id', 'chart-area-box')
-    //   .attr('transform', `translate(${margin.left},${margin.top})`);
-    //
-    // g.append('g')
-    //   .attr('class', 'x-axis')
-    //   .attr("transform", "translate(0," + height + ")")
-    //   .call(d3.axisBottom(x));
-    //
-    // g.append('g')
-    //   .attr('class', 'y-axis')
-    //   .call(d3.axisLeft(y).ticks(10))
-    //   .append('text')
-    //   .attr('class', 'title')
-    //   .text('Anzahl Bauernhöfe')
-    //   .attr('transform', `translate(-50,${height / 2}) rotate(-90)`)
-    //
-    // const barsGroup = g.selectAll('rect')
-    //   .attr('class', 'bar-container')
-    //   .data(data)
-    //   .enter();
-    //
-    // barsGroup.append('rect')
-    //   .attr('class', 'bar')
-    //   .attr('x', d => x(d.label))
-    //   .attr('y', d => y(d.minYearData))
-    //   .attr('width', x.bandwidth() / 2)
-    //   .attr('height', d => height - y(d.minYearData));
-    //
-    // barsGroup.append('rect')
-    //   .attr('class', 'bar')
-    //   .attr('x', d => x(d.label) + x.bandwidth() / 2)
-    //   .attr('y', d => y(d.maxYearData))
-    //   .attr('width', x.bandwidth() / 2)
-    //   .attr('height', d => height - y(d.maxYearData));
+    const {data} = this.props;
 
+    const margin = {top: 50, right: 20, bottom: 40, left: 100};
 
-    // Source: https://bl.ocks.org/mbostock/3887051
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
-
-    const oldData = [
-      data.map(d => d.minYearData),
-      data.map(d => d.maxYearData)
+    const processedData = [
+      this.props.data.map(d => d.minYearData),
+      this.props.data.map(d => d.maxYearData)
     ];
 
-    const nofCategories = oldData[0].length, // number of samples
-      nofGroups = oldData.length; // number of series
+    const colorScale = d3.scaleOrdinal()
+      .range(["#8a89a6", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-    const margin = {top: 20, right: 30, bottom: 30, left: 40},
-      width = 600 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-
-    const scaleY = d3.scaleLinear()
-      .domain([0, d3.max(oldData, oldData => d3.max(oldData))])
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(processedData, oldData => d3.max(oldData))])
       .range([height, 0]);
 
-    const scaleX = d3.scaleBand()
-      .domain(d3.range(nofCategories))
-      .range([0, width], .3);
+    const xScale = d3.scaleBand()
+      .domain(data.map(d => d.label))
+      .rangeRound([0, width]);
 
     const scaleWidth = d3.scaleBand()
-      .domain(d3.range(nofGroups))
-      .range([0, scaleX.bandwidth() - 10]);
-
-    const z = d3.scaleOrdinal()
-      .range(["#8a89a6", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+      .domain(d3.range(processedData.length))
+      .range([0, xScale.bandwidth()])
+      .paddingInner(0.02)
+      .paddingOuter(0.2);
 
     const svg = d3.select(".FarmSizeRelationsChart").append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -107,17 +43,30 @@ export default class FarmSizeRelationsChart extends React.Component {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     svg.append("g").selectAll("g")
-      .data(oldData)
+      .data(processedData)
       .enter().append("g")
-      .style("fill", (d, i) => z(i))
+      .style("fill", (d, i) => colorScale(i))
       .attr("transform", (d, i) => "translate(" + scaleWidth(i) + ",0)")
       .selectAll("rect")
       .data(d => d)
       .enter().append("rect")
       .attr("width", scaleWidth.bandwidth())
-      .attr("height", d => height - scaleY(d))
-      .attr("x", (d, i) => scaleX(i))
-      .attr("y", d => scaleY(d));
+      .attr("height", d => height - yScale(d))
+      .attr("x", (d, i) => xScale(data[i].label))
+      .attr("y", d => yScale(d));
+
+    svg.append('g')
+      .attr('class', 'x-axis')
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale));
+
+    svg.append('g')
+      .attr('class', 'y-axis')
+      .call(d3.axisLeft(yScale))
+      .append('text')
+      .attr('class', 'title')
+      .text('Anzahl Bauernhöfe')
+      .attr('transform', `translate(-80,${height / 2}) rotate(-90)`)
   }
 
   componentDidMount() {
