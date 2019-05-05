fbf2dfa3eef544d1a9109444ea75fef0d4366309
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-05-05 15:27:14 +0200

    Added imgs to show relations of farm sizes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import FarmImg from "../res/imgs/FarmImg";
+import cowImg from "../res/imgs/cow.svg";
+import wheatImg from "../res/imgs/wheat.svg";
+import {Image} from "react-bootstrap";
 
 export default class Header extends React.Component {
 
@@ -33,7 +36,46 @@ export default class Header extends React.Component {
             <p>Die grössten Betriebe haben eine Fläche von 30 Hektaren oder mehr.</p>
           </div>
         </div>
+
+        <h2>Anbaumöglichkeiten pro Hektar</h2>
+
+        <h3>Kühe</h3>
+        <p>In der Schweiz reicht ein Hektar zum ernähren von rund 2 Kühen.</p>
+
+        <div className='horizontal-container'>
+          <div className='relations-container'>
+            <p>In Kleinbetrieben können demnach bis zu 20 Kühe pro Hof gehalten werden.</p>
+            {this.printIcons('cowImg', cowImg, 20)}
+          </div>
+          <div className='relations-container'>
+            <p>Auf Grossbetrieben werden beachtliche Herden von 60 Tieren oder mehr gehalten.</p>
+            {this.printIcons('cowImg', cowImg, 60)}
+          </div>
+        </div>
+
+        <h3>Weizen</h3>
+        <p>Auf einem Hektar Ackerland können 6 Tonnen Weizen angebaut werden.</p>
+
+
+        <div className='horizontal-container'>
+          <div className='relations-container'>
+            <p>Kleinbetriebe reichen demnach um bis zu 60 Tonnen Weizen anzubauen.</p>
+            {this.printIcons('wheatImg', wheatImg, 60)}
+          </div>
+          <div className='relations-container'>
+            <p>Grossbetriebe ermöglichen den Anbau von über 180 Tonnen.</p>
+            {this.printIcons('wheatImg', wheatImg, 180)}
+          </div>
+        </div>
       </div>
     )
+  }
+
+  printIcons(name, type, count) {
+    let container = [];
+    for (let i = 0; i < count; i++) {
+      container[i] = <Image src={type} />
+    }
+    return <div className={name}>{container}</div>;
   }
 }
