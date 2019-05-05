f7c8f6287aa53a69f74bc0da708d377b31214f78
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-05-05 14:24:46 +0200

    Added farm size relations in Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import FarmImg from "../res/imgs/FarmImg";
 
 export default class Header extends React.Component {
 
@@ -10,6 +10,29 @@ export default class Header extends React.Component {
 
         <p>Immer wieder liest man davon, dass für die Schweizer Bäuerinnen und Bauer die Existenz gefährdet ist:
           Für viele Betriebe lohnt sich die Landwirtschft nicht mehr und sie verschwinden.</p>
+
+        <h2>Grössenkategorien</h2>
+
+        <div className='horizontal-container top'>
+            <FarmImg width={'80px'} fill={'#4ec291'}/>
+            <FarmImg width={'100px'} fill={'#42a3f1'}/>
+            <FarmImg width={'120px'} fill={'#e396d1'}/>
+        </div>
+
+        <div className='horizontal-container bottom'>
+          <div>
+            <h3>kleine Höfe</h3>
+            <p>Beuernhöfe, die eine Betriebsfläche von weniger als 10 Hektare haben.</p>
+          </div>
+          <div>
+            <h3>mittlere Höfe</h3>
+            <p>Mittelgrosse Höfe haben eine Fläche zwischen 10 und 30 Hektaren.</p>
+          </div>
+          <div>
+            <h3>grosse Höfe</h3>
+            <p>Die grössten Betriebe haben eine Fläche von 30 Hektaren oder mehr.</p>
+          </div>
+        </div>
       </div>
     )
   }
