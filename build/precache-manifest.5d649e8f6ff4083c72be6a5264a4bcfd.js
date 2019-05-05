f5f46cb81963ceb91bafc9040bb28294908b4d51
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-05-05 15:54:40 +0200

    added /build to git and edited README

---
+++ build/precache-manifest.5d649e8f6ff4083c72be6a5264a4bcfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "206e2329217d630f4c4c",
+    "url": "/static/css/main.8f99afd7.chunk.css"
+  },
+  {
+    "revision": "206e2329217d630f4c4c",
+    "url": "/static/js/main.206e2329.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9d0746e58bc93a2dfab6",
+    "url": "/static/js/2.9d0746e5.chunk.js"
+  },
+  {
+    "revision": "693a9c198644cc338f449c9566583900",
+    "url": "/static/media/cow.693a9c19.svg"
+  },
+  {
+    "revision": "4e4ef96c44e921fcd5097024f549ffec",
+    "url": "/static/media/wheat.4e4ef96c.svg"
+  },
+  {
+    "revision": "bfe448e939c502eb9c1b15c9206e6931",
+    "url": "/static/media/farm-sizes.bfe448e9.csv"
+  },
+  {
+    "revision": "57208f88966f68b2c8399024806ba9d4",
+    "url": "/index.html"
+  }
+];
