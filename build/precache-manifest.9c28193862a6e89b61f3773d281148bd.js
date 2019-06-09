1a4eb668e2f12f614421eb70cb92ce7e04231626
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-06-09 15:34:08 +0200

    re-added /build files

---
+++ build/precache-manifest.9c28193862a6e89b61f3773d281148bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1419e181d99242be4a4e",
+    "url": "/static/css/main.5b293db4.chunk.css"
+  },
+  {
+    "revision": "1419e181d99242be4a4e",
+    "url": "/static/js/main.1419e181.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e709a83f651d287ebe72",
+    "url": "/static/js/2.e709a83f.chunk.js"
+  },
+  {
+    "revision": "e2bbf2b31bd14d41a7614e632307a6b2",
+    "url": "/static/media/farm-sizes.e2bbf2b3.csv"
+  },
+  {
+    "revision": "d06c764e542b8f24e789476d4e6797aa",
+    "url": "/static/media/cow.d06c764e.svg"
+  },
+  {
+    "revision": "2e463940347bd0cec7aee48393d19e7c",
+    "url": "/static/media/wheat.2e463940.svg"
+  },
+  {
+    "revision": "1fb345d3caa4cb0aa4291bca9c6a862c",
+    "url": "/index.html"
+  }
+];
